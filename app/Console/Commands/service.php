2579d4633a44d750e35bb56ee9de7452027d1089
Douglas S. Santos <estilowebmasterk@gmail.com>
2022-11-28 02:17:08 -0300

    create command stubs

---
+++ app/Console/Commands/service.php
@@ -0,0 +1,236 @@
+<?php
+
+namespace App\Console\Commands;
+use Illuminate\Console\Command;
+
+class service extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:service {service?} {--mode=} {--contract=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    protected $register = "public function register(){";
+
+    protected $path = [];
+
+    protected $set = [
+      "Repositories" => ["prefix"=>"Repository", "stub"=>"Repository"],
+      "Services" => ["prefix"=>"Service", "stub"=>"Service"],
+      "Contracts" => ["prefix"=>"RepositoryInterface", "stub"=>"Contract"],
+      "Controllers" => ["prefix"=>"Controller", "stub"=>"Controller"]
+    ];
+
+    protected $typeMakeProvider = "stub"; // stub or shell;
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->path['Configs'] = str_replace("app\Console\Commands", "config\\", __DIR__);
+        $this->path['Providers'] = str_replace("Console\Commands", "Providers\\", __DIR__);
+        $this->path['Contracts'] = str_replace("Console\Commands", "Contracts\\", __DIR__);
+        $this->path['Services'] = str_replace("Console\Commands", "Services\\", __DIR__);
+        $this->path['Repositories'] = str_replace("Console\Commands", "Repositories\\", __DIR__);
+        $this->path['Controllers'] = str_replace("Console\Commands", "Http\Controllers\\", __DIR__);
+        $this->path['Stubs'] = str_replace("Console\Commands", "Stubs\\", __DIR__);
+
+    }
+
+    public function handle()
+    {
+        $name = $this->argument("service");
+        $mode = $this->option("mode");
+        $contract = $this->option("contract");
+        if($mode !== null && ($mode !== "controller" && $mode !== "api")){
+            $this->error("Insert mode, the default is --mode=controller or setter --mode=api for create controller api");
+            return false;
+        }
+        if($contract !== null && ($contract !== "false" && $contract !== "true")){
+            $this->error("Insert Contract, the default is --contract=true or setter --contract=false for disable create interface");
+            return false;
+        }
+
+        if(strtolower(trim($this->option("mode"))) === "api") {
+            $this->set['Controllers']['stub'] = "ControllerApi";
+            $this->path['Controllers'] = str_replace("Console\Commands", "Http\Controllers\api\\", __DIR__);
+        }
+        if(strtolower(trim($this->option("contract"))) === 'false') {
+            $this->set['Repositories']['stub'] = "RepositoryNoContract";
+            unset($this->set['Contracts']);
+        }
+
+        $this->makeDirIfNotExists();
+
+        $this->call("make:stub");
+        if($this->Provider()){
+
+            $this->createDefault($this->path['Repositories'], "AbstractRepository", "Default Repository", "DefaultRepository");
+            $this->createDefault($this->path['Contracts'], "RepositoryInterface", "Default Contract", "DefaultRepositoryInterface");
+            $this->setBind("App\\Contracts\\RepositoryInterface", "App\\Repositories\\AbstractRepository");
+
+            if(empty(trim($name)) || $name === "default"){
+                $this->info("Finish!");
+                return true;
+            }
+
+            if($this->create($name)){
+                if(strtolower(trim($this->option("contract"))) !== 'false') {
+                    if ($this->setBind("App\\Contracts\\{$name}RepositoryInterface", "App\\Repositories\\{$name}Repository")) {
+                        $this->info("Finish!");
+                    }
+                }else{
+                    $this->info("Finish!");
+                }
+
+            }
+
+        }
+        return true;
+    }
+
+    public function Provider(): bool
+    {
+        $fileProvider = $this->path['Providers']."BindServiceProvider.php";
+        if(!file_exists($fileProvider)){
+
+            if($this->typeMakeProvider === "shell") {
+                $this->comment("Creating provider: BindServiceProvider");
+                $makeProvider = shell_exec("php artisan make:provider BindServiceProvider");
+            }else{
+                $makeProvider = $this->createDefault($this->path['Providers'], "BindServiceProvider", "provider", "BindServiceProvider");
+            }
+
+            if($makeProvider!== false) {
+
+                $fileConfigApp = $this->path['Configs']."app.php";
+                $ConfigRead = file_get_contents($fileConfigApp);
+
+                $openFile = fopen($fileConfigApp, "w+");
+                $publishAfter = "App\Providers\RouteServiceProvider::class,";
+                $publishBefore = "App\Providers\BindServiceProvider::class,";
+
+                if(file_exists($fileConfigApp) && !str_contains($ConfigRead, $publishBefore)) {
+                    $configPublishProvider = $this->concat($publishAfter, $publishBefore, $ConfigRead);
+                    if (fwrite($openFile, $configPublishProvider)) {
+                        $this->info("Provider published in: {$fileConfigApp}");
+                        fclose($openFile);
+                        return true;
+                    }
+
+                }
+
+                $this->info("Provider created in: {$fileProvider}");
+            }
+        }
+        return file_exists($fileProvider);
+    }
+
+    public function setBind($interface, $class)
+    {
+        $provider = $this->path['Providers']."BindServiceProvider.php";
+
+        $bind = "App::bind('{$interface}','{$class}');";
+
+        $providerRead = file_get_contents($provider);
+        $bindPublished = str_replace($this->register, $this->register."\n\t\t".$bind, $providerRead);
+
+        if(file_exists($provider) && !str_contains($providerRead, $bind)){
+            $this->comment("Publishing bind interface: {$interface}");
+            $openFile = fopen($provider, "w+");
+            if(fwrite($openFile, $bindPublished)) {
+                $this->info("Bind published in: {$provider}");
+                fclose($openFile);
+                return true;
+            }
+        }else{
+            $this->comment("The bind is already published on the provider: {$interface}");
+            return true;
+        }
+    }
+
+    public function createDefault($path, $name, $title, $stub)
+    {
+        $fileDefault = $path."{$name}.php";
+        if(!file_exists($fileDefault)){
+
+            $this->comment("Creating {$title}: {$name}");
+            $openFile = fopen($fileDefault, "w+");
+            $readStubsInterface = file_get_contents($this->path['Stubs']."{$stub}.stub");
+            if(fwrite($openFile, $readStubsInterface)) {
+                $this->info("{$title} created in: {$fileDefault}");
+                fclose($openFile);
+            }
+        }
+        return file_exists($fileDefault);
+    }
+
+    public function create($name)
+    {
+        foreach ($this->set as $key => $parameters) {
+            $prefix = ($parameters['prefix'] ?? $key);
+            $path = isset($parameters['path']) ? $this->path[@$parameters['path']] : $this->path[$key];
+
+            $fileCreatePath = "{$path}{$name}{$prefix}.php";
+
+            if (!file_exists($fileCreatePath)) {
+
+                $this->comment("Creating {$key}: {$name}{$prefix}");
+
+                $readStubs = file_get_contents($this->path['Stubs'] . ($parameters['stub'] ?? $key).".stub");
+                $replacesVariableInStubs = str_replace('{$name}', $name, $readStubs);
+
+                $openFile = fopen($fileCreatePath, "w+");
+                if (fwrite($openFile, $replacesVariableInStubs)) {
+                    $this->info("{$key} created in: {$fileCreatePath}");
+                    fclose($openFile);
+                }else{
+                    $this->error("Failed to create the {$key} in: {$fileCreatePath}");
+                    return false;
+                }
+
+            }else{
+                $this->comment("{$name}{$prefix} is already created.");
+            }
+        }
+        return true;
+    }
+
+    public function concat($strPrimary, $strSecond, $string)
+    {
+        $concat = str_replace($strPrimary, $strPrimary."\n\t\t".$strSecond, $string);
+
+        return $concat;
+
+    }
+
+    public function makeDirIfNotExists()
+    {
+        $show = false;
+        foreach ($this->path as $path) {
+            if(!is_dir($path)){
+                if(!$show){
+                    $this->info("Creating folders.");
+                    $show = true;
+                }
+                mkdir($path);
+            }
+        }
+        if($show)$this->info("Finish!");
+    }
+
+}
---
+++ app/Console/Commands/stubs.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\Isolatable;
+
+class stubs extends Command implements Isolatable
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:stub';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+    protected $path;
+
+    protected $runStubs;
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->path['Stubs'] = str_replace("Console\Commands", "Stubs\\", __DIR__);
+
+        $this->runStubs = [
+          "Provide"=>$this->BindServiceProvider(),
+          "Contract"=>$this->Contract(),
+          "DefaultRepository"=>$this->DefaultRepository(),
+          "DefaultRepositoryInterface"=>$this->DefaultRepositoryInterface(),
+          "Repository"=>$this->Repository(),
+          "RepositoryNoContract"=>$this->RepositoryNoContract(),
+          "Service"=>$this->Service(),
+          "Controller"=>$this->Controller(),
+          "ControllerApi"=>$this->ControllerApi(),
+        ];
+
+    }
+
+    public function handle()
+    {
+        //$name = $this->argument("service");
+        $show = false;
+        foreach ($this->runStubs as $stub) {
+            if(!file_exists($this->path['Stubs'].$stub['file'].".stub")) {
+                if(!$show){
+                    $this->info("Creating stubs.");
+                    $show = true;
+                }
+                $this->create($stub);
+            }
+        }
+        if($show)$this->info("Finish!");
+
+    }
+
+    private function BindServiceProvider(): array
+    {
+        $content = "<?php\n\nnamespace App\Providers;\n\nuse Illuminate\Support\Facades\App;\nuse Illuminate\Support\ServiceProvider;
+\nclass BindServiceProvider extends ServiceProvider\n{\n\t/**\n\t * Register services.\n\t *\n\t * @return void\n\t */
+\n\tpublic function register(){\n\n\t}\n\n\t/**\n\t * Bootstrap services.\n\t *\n\t * @return void\n\t */
+\n\tpublic function boot()\n\t{\n\n\t}\n}";
+        return ["file"=>"BindServiceProvider", 'content'=>$content];
+    }
+
+    private function Contract(): array
+    {
+        $content = "<?php\n\nnamespace App\Contracts;\n\ninterface {\$name}RepositoryInterface\n{\n\tpublic function getAll();\n\tpublic function find(\$id);
+\tpublic function create(\$data);\n\tpublic function update(\$data, \$id);\n\tpublic function destroy(\$id);\n}";
+        return ["file"=>"Contract", 'content'=>$content];
+    }
+
+    private function DefaultRepository(): array
+    {
+        $content = "<?php\n\nnamespace App\Repositories;\n\nuse App\Contracts\RepositoryInterface;\n\nclass AbstractRepository implements RepositoryInterface\n{
+\n\tpublic function getAll()\n\t{\n\t\t// TODO: Implement getAll() method.\n\t}
+\n\tpublic function find(\$id)\n\t{\n\t\t// TODO: Implement find() method.\n\t}
+\n\tpublic function create(\$data)\n\t{\n\t\t// TODO: Implement create() method.\n\t}
+\n\tpublic function update(\$data, \$id)\n\t{\n\t\t// TODO: Implement update() method.\n\t}
+\n\tpublic function destroy(\$id)\n\t{\n\t\t// TODO: Implement destroy() method.\n\t}\n}";
+        return ["file"=>"DefaultRepository", 'content'=>$content];
+    }
+
+    private function DefaultRepositoryInterface(): array
+    {
+        $content = "<?php\n\nnamespace App\Contracts;\n\ninterface RepositoryInterface\n{
+\tpublic function getAll();\n\tpublic function find(\$id);\n\tpublic function create(\$data);\n\tpublic function update(\$data, \$id);
+\tpublic function destroy(\$id);\n}";
+        return ["file"=>"DefaultRepositoryInterface", 'content'=>$content];
+    }
+
+    private function Repository(): array
+    {
+        $content = "<?php\n
+namespace App\Repositories;\n\nuse App\Contracts\{\$name}RepositoryInterface;\nuse App\Models\{\$name};\n
+class {\$name}Repository implements {\$name}RepositoryInterface\n{\n\tprotected {\$name} \$model;\n
+\tpublic function __construct({\$name} \$model)\n\t{\n\t\t\$this->model = \$model;\n\t}\n
+\tpublic function getAll(): \Illuminate\Http\JsonResponse\n\t{
+\t\t\$all = \$this->model->all();\n\t\treturn response()->json([\"status\" => \$all->count() != 0, \"data\" => \$all->toArray()]);\n\t}\n
+\tpublic function find(\$id): \Illuminate\Http\JsonResponse\n\t{
+\t\t\$ids = explode(\",\", \$id);\n\t\t\$find = \$this->model->whereIn('id', \$ids);
+\t\treturn response()->json([\"status\" => \$find->exists(), \"ids\" => \$ids, \"data\" => \$find->get()]);\n\t}\n
+\tpublic function create(\$data): \Illuminate\Http\JsonResponse\n\t{
+\t\t\$create = \$this->model->create(\$data);\n\t\treturn response()->json([\"status\" => \$create !== false, \"data\" => \$create]);\n\t}\n
+\tpublic function update(\$data, \$id): \Illuminate\Http\JsonResponse\n\t{
+\t\t\$update = \$this->model->where('id', \$id)->update(\$data);
+\t\treturn response()->json([\"status\" => \$update != 0, \"id\" => \$update != 0 ? \$id : null, \"data\" => \$update != 0 ? \$data : null]);\n\t}\n
+\tpublic function destroy(\$id): \Illuminate\Http\JsonResponse\n\t{\n\t\t\$destroy = \$this->model->destroy(\$id);
+\t\treturn response()->json([\"status\" => \$destroy !== 0, \"result\" => \$destroy]);\n\t}\n}";
+        return ["file"=>"Repository", 'content'=>$content];
+    }
+
+    private function RepositoryNoContract(): array
+    {
+        $content = "<?php\n
+namespace App\Repositories;\n\nuse App\Contracts\{\$name}RepositoryInterface;\nuse App\Models\{\$name};\n
+class {\$name}Repository\n{\n\tprotected {\$name} \$model;\n
+\tpublic function __construct({\$name} \$model)\n\t{\n\t\t\$this->model = \$model;\n\t}\n
+\tpublic function getAll(): \Illuminate\Http\JsonResponse\n\t{
+\t\t\$all = \$this->model->all();\n\t\treturn response()->json([\"status\" => \$all->count() != 0, \"data\" => \$all->toArray()]);\n\t}\n
+\tpublic function find(\$id): \Illuminate\Http\JsonResponse\n\t{
+\t\t\$ids = explode(\",\", \$id);\n\t\t\$find = \$this->model->whereIn('id', \$ids);
+\t\treturn response()->json([\"status\" => \$find->exists(), \"ids\" => \$ids, \"data\" => \$find->get()]);\n\t}\n
+\tpublic function create(\$data): \Illuminate\Http\JsonResponse\n\t{
+\t\t\$create = \$this->model->create(\$data);\n\t\treturn response()->json([\"status\" => \$create !== false, \"data\" => \$create]);\n\t}\n
+\tpublic function update(\$data, \$id): \Illuminate\Http\JsonResponse\n\t{
+\t\t\$update = \$this->model->where('id', \$id)->update(\$data);
+\t\treturn response()->json([\"status\" => \$update != 0, \"id\" => \$update != 0 ? \$id : null, \"data\" => \$update != 0 ? \$data : null]);\n\t}\n
+\tpublic function destroy(\$id): \Illuminate\Http\JsonResponse\n\t{\n\t\t\$destroy = \$this->model->destroy(\$id);
+\t\treturn response()->json([\"status\" => \$destroy !== 0, \"result\" => \$destroy]);\n\t}\n}";
+        return ["file"=>"RepositoryNoContract", 'content'=>$content];
+    }
+
+    private function Service(): array
+    {
+        $content = "<?php\n\nnamespace App\Services;\n\n\nuse App\Repositories\{\$name}Repository;\n
+class {\$name}Service\n{\n\tprotected {\$name}Repository \$repos;\n\n
+\n\tpublic function __construct({\$name}Repository \$repos)\n\t{\n\t\t\$this->repos = \$repos;\n\t}\n\n
+\n\tpublic function getAll(): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->repos->getAll();\n\t}\n\n
+\n\tpublic function find(\$id): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->repos->find(\$id);\n\t}\n\n
+\n\tpublic function create(\$data): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->repos->create(\$data);\n\t}\n\n
+\n\tpublic function update(\$data, \$id): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->repos->update(\$data, \$id);\n\t}\n\n
+\n\tpublic function destroy(\$id): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->repos->destroy(\$id);\n\t}\n}";
+        return ["file"=>"Service", 'content'=>$content];
+    }
+
+    private function Controller(): array
+    {
+        $content = "<?php\n\nnamespace App\Http\Controllers;\n\nuse App\Services\{\$name}Service;\nuse Illuminate\Http\Request;\n
+class {\$name}Controller extends Controller\n{\n\tpublic {\$name}Service \$service;\n\n\tpublic function __construct({\$name}Service \$service)\n\t{\n\t\t\$this->service = \$service;\n\t}
+\tpublic function getAll(): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->service->getAll();\n\t}
+\tpublic function find(\$id): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->service->find(\$id);\n\t}
+\tpublic function create(Request \$request): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->service->create(\$request->all());\n\t}
+\tpublic function update(Request \$request, \$id): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->service->update(\$request->all(), \$id);\n\t}
+\tpublic function destroy(\$id): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->service->destroy(\$id);\n\t}\n}";
+        return ["file"=>"Controller", 'content'=>$content];
+    }
+
+    private function ControllerApi(): array
+    {
+        $content = "<?php\n\nnamespace App\Http\Controllers\api;\n\nuse App\Http\Controllers\Controller;\nuse App\Services\{\$name}Service;\nuse Illuminate\Http\Request;\n
+class {\$name}Controller extends Controller\n{\n\tpublic {\$name}Service \$service;\n\n\tpublic function __construct({\$name}Service \$service)\n\t{\n\t\t\$this->service = \$service;\n\t}
+\tpublic function getAll(): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->service->getAll();\n\t}
+\tpublic function find(\$id): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->service->find(\$id);\n\t}
+\tpublic function create(Request \$request): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->service->create(\$request->all());\n\t}
+\tpublic function update(Request \$request, \$id): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->service->update(\$request->all(), \$id);\n\t}
+\tpublic function destroy(\$id): \Illuminate\Http\JsonResponse\n\t{\n\t\treturn \$this->service->destroy(\$id);\n\t}\n}";
+        return ["file"=>"ControllerApi", 'content'=>$content];
+    }
+
+    private function create($stub)
+    {
+        $fileStub = $this->path['Stubs'].$stub['file'].".stub";
+        if(!file_exists($fileStub)) {
+            $fileCreate = fopen($fileStub, "w+");
+            $this->comment("Creating stub: " . $stub['file']);
+            if (fwrite($fileCreate, $stub['content'])) {
+                $this->info("Stub " . $stub['file'] . " created in: " . $fileStub);
+                fclose($fileCreate);
+            }
+        }else{
+            //$this->comment($stub['file']." is already created.");
+        }
+    }
+
+
+
+
+
+}
