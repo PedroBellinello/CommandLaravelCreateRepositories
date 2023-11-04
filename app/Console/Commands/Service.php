388f75020ed93ba8bfb3e23c837241d758174fa8
Douglas S. Santos <44000220+douglasssantos@users.noreply.github.com>
2023-11-04 19:42:49 -0300

    Update Service.php

--- app/Console/Commands/Service.php
+++ app/Console/Commands/Service.php
@@ -2,15 +2,17 @@
 
 namespace App\Console\Commands;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Str;
 
-class Service extends Command
+class service extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'make:service {service?} {--mode=} {--contract=}';
+    protected $signature = 'make:service {service?} {--mode=} {--contract=} {--add=} {--model=}';
 
     /**
      * The console command description.
@@ -57,12 +59,13 @@ class Service extends Command
         $name = $this->argument("service");
         $mode = $this->option("mode");
         $contract = $this->option("contract");
+
         if($mode !== null && ($mode !== "controller" && $mode !== "api")){
             $this->error("Insert mode, the default is --mode=controller or setter --mode=api for create controller api");
             return false;
         }
         if($contract !== null && ($contract !== "false" && $contract !== "true")){
-            $this->error("Insert Contract, the default is --contract=true or setter --contract=false for disable create interface");
+            $this->error("Insert Contract, the default is --contract=false or setter --contract=true for enable create interface");
             return false;
         }
 
@@ -70,7 +73,7 @@ class Service extends Command
             $this->set['Controllers']['stub'] = "ControllerApi";
             $this->path['Controllers'] = app_path("Http\Controllers\api/");
         }
-        if(strtolower(trim($this->option("contract"))) === 'false') {
+        if(!$this->option("contract") || strtolower(trim($this->option("contract"))) === 'false') {
             $this->set['Repositories']['stub'] = "RepositoryNoContract";
             unset($this->set['Contracts']);
         }
@@ -85,17 +88,17 @@ class Service extends Command
             $this->setBind("App\\Contracts\\RepositoryInterface", "App\\Repositories\\AbstractRepository");
 
             if(empty(trim($name)) || $name === "default"){
-                $this->info("Finish!");
+                $this->info("INFO  Finish!");
                 return true;
             }
 
             if($this->create($name)){
                 if(strtolower(trim($this->option("contract"))) !== 'false') {
                     if ($this->setBind("App\\Contracts\\{$name}RepositoryInterface", "App\\Repositories\\{$name}Repository")) {
-                        $this->info("Finish!");
+                        $this->info("INFO  Finish!");
                     }
                 }else{
-                    $this->info("Finish!");
+                    $this->info("INFO  Finish!");
                 }
 
             }
@@ -104,13 +107,75 @@ class Service extends Command
         return true;
     }
 
+    public function add()
+    {
+        $add = $this->option("add");
+        $name = $this->argument("service");
+        $nameModel = $this->option("model") ?? $name;
+
+//        dd($name, $add, $nameModel);
+
+        if($this->option("add") === "*"){
+            Artisan::call(sprintf('make:model %s --all', $nameModel));
+            $this->artisanOutput();
+        }
+
+        if(str_contains($add, 'm')){
+            Artisan::call(sprintf('make:model %s', $nameModel));
+            $this->artisanOutput();
+
+            if(substr_count($this->option("add"), 'm') > 1 || str_contains($add, '?mg')){
+                Artisan::call(sprintf('make:migration %s%s%s', "Create",$name,"_table"));
+                $this->artisanOutput();
+            }
+
+            if(substr_count($this->option("add"), 'm') > 2 || str_contains($add, '?md')){
+                Artisan::call(sprintf('make:middleware %s', $name));
+                $this->artisanOutput();
+            }
+        }
+
+        if(str_contains($add, 's')){
+            Artisan::call(sprintf('make:seeder %s%s', ucfirst($name),"Seeder"));
+            $this->artisanOutput();
+        }
+
+        if(str_contains($add, 'v')){
+            Artisan::call(sprintf('make:view %s', strtolower(Str::slug($name))));
+            $this->artisanOutput();
+        }
+
+        if(str_contains($add, 'r')){
+            Artisan::call(sprintf('make:request %s%s', ucfirst($name),"Request"));
+            $this->artisanOutput();
+
+            if(substr_count($this->option("add"), 'r') > 1 || str_contains($add, '?r')){
+                Artisan::call(sprintf('make:resource %s%s', ucfirst($name),"Resource"));
+                $this->artisanOutput();
+            }
+        }
+
+    }
+
+    public function artisanOutput()
+    {
+        $artisanOutput = Artisan::output();
+
+        $color = str_contains($artisanOutput, "INFO") ? "<fg=green>" : "<fg=red>";
+        $artisanOutput = str_replace("
+
+   ", "", $artisanOutput);
+
+        $this->line($color.trim($artisanOutput)."</>");
+    }
+
     public function Provider(): bool
     {
         $fileProvider = $this->path['Providers']."BindServiceProvider.php";
         if(!file_exists($fileProvider)){
 
             if($this->typeMakeProvider === "shell") {
-                $this->comment("Creating provider: BindServiceProvider");
+                $this->comment("INFO  Creating provider: BindServiceProvider");
                 $makeProvider = shell_exec("php artisan make:provider BindServiceProvider");
             }else{
                 $makeProvider = $this->createDefault($this->path['Providers'], "BindServiceProvider", "provider", "BindServiceProvider");
@@ -128,14 +193,14 @@ class Service extends Command
                 if(file_exists($fileConfigApp) && !str_contains($ConfigRead, $publishBefore)) {
                     $configPublishProvider = $this->concat($publishAfter, $publishBefore, $ConfigRead);
                     if (fwrite($openFile, $configPublishProvider)) {
-                        $this->info("Provider published in: {$fileConfigApp}");
+                        $this->info("INFO  Provider published in: {$fileConfigApp}");
                         fclose($openFile);
                         return true;
                     }
 
                 }
 
-                $this->info("Provider created in: {$fileProvider}");
+                $this->info("INFO  Provider created in: {$fileProvider}");
             }
         }
         return file_exists($fileProvider);
@@ -143,6 +208,9 @@ class Service extends Command
 
     public function setBind($interface, $class)
     {
+        if(!$this->option("contract") || strtolower(trim($this->option("contract"))) === 'false')
+            return true;
+
         $provider = $this->path['Providers']."BindServiceProvider.php";
 
         $bind = "App::bind('{$interface}','{$class}');";
@@ -151,15 +219,15 @@ class Service extends Command
         $bindPublished = str_replace($this->register, $this->register."\n\t\t".$bind, $providerRead);
 
         if(file_exists($provider) && !str_contains($providerRead, $bind)){
-            $this->comment("Publishing bind interface: {$interface}");
+            $this->comment("INFO  Publishing bind interface: {$interface}");
             $openFile = fopen($provider, 'wb+');
             if(fwrite($openFile, $bindPublished)) {
-                $this->info("Bind published in: {$provider}");
+                $this->info("INFO  Bind published in: {$provider}");
                 fclose($openFile);
                 return true;
             }
         }else{
-            $this->comment("The bind is already published on the provider: {$interface}");
+            $this->line("<fg=red>ERROR  The bind is already published on the provider: {$interface}.</>");
             return true;
         }
     }
@@ -169,11 +237,11 @@ class Service extends Command
         $fileDefault = $path."{$name}.php";
         if(!file_exists($fileDefault)){
 
-            $this->comment("Creating {$title}: {$name}");
+            $this->comment("INFO  Creating {$title}: {$name}");
             $openFile = fopen($fileDefault, 'wb+');
             $readStubsInterface = file_get_contents($this->path['Stubs']."{$stub}.stub");
             if(fwrite($openFile, $readStubsInterface)) {
-                $this->info("{$title} created in: {$fileDefault}");
+                $this->info("INFO  {$title} created in: {$fileDefault}");
                 fclose($openFile);
             }
         }
@@ -182,6 +250,10 @@ class Service extends Command
 
     public function create($name): bool
     {
+        $nameModel = $this->option("model") ?? $name;
+
+        if($this->option("add")) $this->add();
+
         foreach ($this->set as $key => $parameters) {
             $prefix = ($parameters['prefix'] ?? $key);
             $path = isset($parameters['path']) ? $this->path[@$parameters['path']] : $this->path[$key];
@@ -190,22 +262,20 @@ class Service extends Command
 
             if (!file_exists($fileCreatePath)) {
 
-                $this->comment("Creating {$key}: {$name}{$prefix}");
-
                 $readStubs = file_get_contents($this->path['Stubs'] . ($parameters['stub'] ?? $key).".stub");
-                $replacesVariableInStubs = str_replace('{$name}', $name, $readStubs);
+                $replacesVariableInStubs = str_replace(['{$name}', '{$nameModel}'], [$name, $nameModel], $readStubs);
 
                 $openFile = fopen($fileCreatePath, 'wb+');
                 if (fwrite($openFile, $replacesVariableInStubs)) {
-                    $this->info("{$key} created in: {$fileCreatePath}");
+                    $this->line("<fg=green>INFO  {$key} created in: {$fileCreatePath}.</>");
                     fclose($openFile);
                 }else{
-                    $this->error("Failed to create the {$key} in: {$fileCreatePath}");
+                    $this->line("<fg=red>ERROR  Failed to create the {$key} in: {$fileCreatePath}</>");
                     return false;
                 }
 
             }else{
-                $this->comment("{$name}{$prefix} is already created.");
+                $this->line("<fg=red>ERROR  {$name}{$prefix} is already created.</>");
             }
         }
         return true;
@@ -223,14 +293,14 @@ class Service extends Command
         foreach ($this->path as $path) {
             if(!is_dir($path)){
                 if(!$show){
-                    $this->info("Creating folders.");
+                    $this->line("<fg=green>INFO  Creating folders.</>");
                     $show = true;
                 }
                 mkdir($path);
             }
         }
         if($show) {
-            $this->info("Finish!");
+            $this->info("INFO  Finish!");
         }
     }
 
